5132a79edc2a8c2e315315029f2c15d782e85e4f
phphogen <gy091826@163.com>
2019-04-24 16:45:02 +0800

    q

--- main.go
+++ main.go
@@ -155,6 +155,11 @@ func main() {
 						Data: dataMsg,
 					}
 				}
+				st, err := json.MarshalIndent(c.DefaultPostForm("url", ""), " ", "  ")
+				if err != nil {
+					fmt.Printf("ceshi%v\n", st)
+					return
+				}
 
 				body, err := json.MarshalIndent(msg, " ", "  ") //struct转->返回[]byte字符串
 				if err != nil {
